c2a74ec2d569c97740d6d61965d8acaffee9b731
Gabe Scholz <scholz.gabe@gmail.com>
2021-10-05 12:35:27 -0700

    Compile interface

--- src/compile.ts
+++ src/compile.ts
@@ -11,11 +11,25 @@ const defaultOptions: Options = {
   cwd: process.cwd(),
 };
 
-export const compile = async <T>(
+export function compile(schema: JSONSchema7, transformers: [], options?: Options): Promise<[]>; // prettier-ignore
+export function compile<T>(schema: JSONSchema7, transformers: [Transformer<T>], options?: Options): Promise<[T]>; // prettier-ignore
+export function compile<T, U>(schema: JSONSchema7, transformers: [Transformer<T>, Transformer<U>], options?: Options): Promise<[T, U]>; // prettier-ignore
+export function compile<T, U, V>(schema: JSONSchema7, transformers: [Transformer<T>, Transformer<U>, Transformer<V>], options?: Options): Promise<[T, U, V]>; // prettier-ignore
+export function compile<T, U, V, W>(schema: JSONSchema7, transformers: [Transformer<T>, Transformer<U>, Transformer<V>, Transformer<W>], options?: Options): Promise<[T, U, V, W]>; // prettier-ignore
+export function compile<T, U, V, W, X>(schema: JSONSchema7, transformers: [Transformer<T>, Transformer<U>, Transformer<V>, Transformer<W>, Transformer<X>], options?: Options): Promise<[T, U, V, W, X]>; // prettier-ignore
+export function compile<T, U, V, W, X, Y>(schema: JSONSchema7, transformers: [Transformer<T>, Transformer<U>, Transformer<V>, Transformer<W>, Transformer<X>, Transformer<Y>], options?: Options): Promise<[T, U, V, W, X, Y]>; // prettier-ignore
+export function compile<T, U, V, W, X, Y, Z>(schema: JSONSchema7, transformers: [Transformer<T>, Transformer<U>, Transformer<V>, Transformer<W>, Transformer<X>, Transformer<Y>, Transformer<Z>], options?: Options): Promise<[T, U, V, W, X, Y, Z]>; // prettier-ignore
+export async function compile(
   schema: JSONSchema7,
-  transformer: Transformer<T>,
+  transformers: Transformer<any>[],
   options = defaultOptions
-): Promise<T> => {
+): Promise<any[]> {
+  if (transformers.length === 0) {
+    return [];
+  }
+
   const ast = await parse(schema, options);
-  return transform(ast, transformer);
-};
+  const proms = transformers.map((transformer) => transform(ast, transformer));
+
+  return Promise.all(proms);
+}
